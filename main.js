598adfda011ac4f73ad9119b85c9077c9cf98aab
RenLevingar <110576801+RenLevingar@users.noreply.github.com>
2023-08-03 15:17:08 -0700

    Home Page and Navbar
    The home page has a new background, one page was also added. In addition, the navbar has progressed to a new state. The new navbar has all pages and a better overall look.

---
+++ main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $(".bg").on(function(){
+      $(".header-bg").css("background-color", "rgba(0,0,190,0.75)");
+      }, function(){
+      $(".header-bg").css("background-color", "");
+    });
+  });

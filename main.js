710fc8e716a522b6544756ae43a9d2d963a4dbf4
RenLevingar <110576801+RenLevingar@users.noreply.github.com>
2023-08-07 15:16:52 -0700

    Pages
    Added 3 parallax scrolling to all pages this give each page a reactive background. As well, each page has a titled card but needs to be fixed internally of what is said and the SCSS needs to be updated for it.

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+// Makes the header fade whenever you scroll
 $(document).ready(function(){
     $(document).on("scroll", pageScroller);
   });
 
   function pageScroller(){
     if($(document).scrollTop() != 0){
-      $(".header-bg").css("background-color", "rgba(0,0,190,0.75)");
+      $(".header-bg").css("background-color", "rgba(0,0,0,0.25)");
     } else {
       $(".header-bg").css("background-color", "");
     }
